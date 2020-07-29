d73a6964965955c37e7dc229ab1bf3231cead647
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-07-29 17:13:13 +0200

    add scores to movies

--- functions/movies.js
+++ functions/movies.js
@@ -1,6 +1,32 @@
+const { URL } = require("url");
+const fetch = require("node-fetch");
 const movies = require("../data/movies.json");
 
+require("dotenv").config();
+
 exports.handler = async () => {
+  const api = new URL("https://www.omdbapi.com/");
+
+  api.searchParams.set("apikey", process.env.OMDB_API_KEY);
+
+  const promises = movies.map((movie) => {
+    // use IMDB id to lookup details
+    api.searchParams.set("i", movie.id);
+
+    return fetch(api)
+      .then((response) => response.json())
+      .then((data) => {
+        const scores = data.Ratings;
+
+        return {
+          ...movie,
+          scores,
+        };
+      });
+  });
+
+  const moviesWithRatings = await Promise.all(promises);
+
   return {
     statusCode: 200,
     body: JSON.stringify(movies),
